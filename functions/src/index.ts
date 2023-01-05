d46472d56843782ab2f8e1cababe21242b0c8e70
VishnuDileesh <munnadileesh@gmail.com>
2023-01-05 14:01:23 +0530

    Add - cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,50 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import {BigQuery} from "@google-cloud/bigquery";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+// pixcam.testcam
+
+admin.initializeApp();
+
+export const fetchDataQuery = functions.https.onCall(async (data, context) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  const PROJECTID_DEV = "pixcam";
+  const DATASETID_DEV = "testcam";
+  const TABLEID_DEV = "testTable";
+
+  const bigquery = new BigQuery();
+
+  //   const dataset = bigqueryClient.dataset(DATASETID_DEV);
+  //   const [table] = await dataset.table(TABLEID_DEV).get();
+
+  console.log("Table:");
+
+
+  const query = "SELECT * FROM pixcam.testcam.testTable LIMIT 10;";
+
+  const options = {
+    query: query,
+    location: "asia-south1",
+    useLegacySql: true,
+    allowLargeResult: true,
+    destinationTable: {
+      projectId: PROJECTID_DEV,
+      datasetId: DATASETID_DEV,
+      tableId: TABLEID_DEV,
+    },
+  };
+
+  const [job] = await bigquery.createQueryJob(options);
+  console.log(`Job ${job.id} started.`);
+
+  const [rows] = await job.getQueryResults();
+  console.log("Rows:");
+  rows.forEach((row) => console.log(row));
+  return [rows];
+
+  //   console.log(table.metadata.tableReference);
+
+//   return [table];
+});
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,32 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+
+FirebaseFunctions functions = FirebaseFunctions.instance;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final db = FirebaseFirestore.instance;
 
   String welcome = 'No data';
 
   @override
-  initState()  {
+  initState() {
     // TODO: implement initState
     super.initState();
   }
 
+  Future<void> getTableData() async {
+    HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('fetchDataQuery');
+    final results = await callable();
+    var itemsList = results.data;
+
+    print(itemsList); // ["Apple", "Banana", "Cherry", "Date", "Fig", "Grapes"]
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -35,15 +43,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+              onPressed: getTableData,
+              child: const Text('Get Data'),
+            ),
             FutureBuilder(
               future: db.collection("title").doc('qbBF47yHDlQ76zacjzUg').get(),
               builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                if(snapshot.hasError) {
+                if (snapshot.hasError) {
                   return Text('Error');
                 }
-                if(snapshot.hasData) {
+                if (snapshot.hasData) {
                   return Text(snapshot.data['name']);
-                }else {
+                } else {
                   return Text('no data');
                 }
               },
