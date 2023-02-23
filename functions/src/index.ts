be2c2c2a1e9f94787f019a580bf518dfba8b9bc3
VishnuDileesh <munnadileesh@gmail.com>
2023-02-23 18:18:46 +0530

    Add - weekly data fetch cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -63,3 +63,39 @@ export const fetchDataQuery = functions.https.onCall(async (data, context) => {
 
   return [rows];
 });
+
+export const fetchWeeklyDataQuery = functions
+    .https.onCall(async (data, context) => {
+      functions.logger.info("Hello logs!", {structuredData: true});
+      console.log("Data: req: ", data);
+
+      const query = `SELECT
+      EXTRACT(DAY FROM TIMESTAMP(countEvent.timeStamp) 
+      AT TIME ZONE "Asia/Kolkata") AS day,
+      COUNT(*) AS total,
+      COUNTIF(countEvent.eventType = "Out") AS outCount,
+      COUNTIF(countEvent.eventType = "In") AS inCount
+    FROM
+      pixcam.testcam.testTable
+    WHERE 
+      TIMESTAMP(countEvent.timeStamp) 
+      BETWEEN TIMESTAMP_SUB(TIMESTAMP(${data["date"]}), 
+      INTERVAL 7 DAY) AND TIMESTAMP(${data["date"]})
+    GROUP BY
+      day`;
+
+      const options = {
+        query: query,
+        location: "asia-south1",
+        useLegacySql: false,
+        allowLargeResult: true,
+      };
+
+      const [job] = await bigquery.createQueryJob(options);
+      console.log(`Job ${job.metadata} started.`);
+
+      const [rows] = await job.getQueryResults();
+
+      return [rows];
+    });
+
